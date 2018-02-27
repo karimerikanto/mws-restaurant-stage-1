9320514e357fbfc7a514e5d76be4f1b8468ced67
karim <kari.merikanto@gmail.com>
2018-02-27 18:36:17 +0200

    feat: Add styles file to be cached in service worker

--- js/sw.js
+++ js/sw.js
@@ -7,6 +7,7 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(staticCacheName).then(function(cache) {
       return cache.addAll([
+        'css/styles.css',
         'js/main.js',
         'js/dbhelper.js',
         'js/restaurant_info.js',
