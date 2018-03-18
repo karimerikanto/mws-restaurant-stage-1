574f3a1656972650404b9fc876e22a50df74f2a4
karim <kari.merikanto@gmail.com>
2018-03-18 09:42:44 +0200

    feat: Change restaurant images to be in webp format

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -105,7 +105,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}.jpg`);
+    return (`/img/${restaurant.photograph}.webp`);
   }
 
   /**
