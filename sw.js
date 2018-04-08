71bfd7ca5539efe6dd66ebc860b1b48774b3e9a6
karim <kari.merikanto@gmail.com>
2018-04-08 15:30:02 +0300

    refactor: Put every cache serse to it's own function in service worker

--- sw.js
+++ sw.js
@@ -9,9 +9,8 @@ const allCacheNames = [
 ];
 
 const remoteCachePathPrefixes = [
-  'https://maps.googleapis.com/maps/api/js?key=AIzaSyB14vP09l2oJojjzpXxQGAtcYHz5caC2IQ&libraries=places&callback=initMap', 
+  'https://maps.googleapis.com/maps/api/js?key=AIzaSyB14vP09l2oJojjzpXxQGAtcYHz5caC2IQ&callback=initMap', 
   'https://maps.googleapis.com/maps-api',
-  'https://fonts.gstatic.com',
   'https://maps.gstatic.com/mapfiles'
   ];
 
@@ -29,7 +28,9 @@ self.addEventListener('install', event => {
         'restaurant.html',
         'image_missing.svg',
         'favorite_on.svg',
-        'favorite_off.svg'
+        'favorite_off.svg',
+        'https://fonts.gstatic.com/s/roboto/v18/KFOmCnqEu92Fr1Mu4mxKKTU1Kg.woff2',
+        'https://fonts.gstatic.com/s/roboto/v18/KFOlCnqEu92Fr1MmEU9fBBc4AMP6lQ.woff2'
       ]);
     })
   );
@@ -40,49 +41,23 @@ self.addEventListener('install', event => {
  */
 self.addEventListener('fetch', event => {
   const requestUrl = new URL(event.request.url);
-
+  
   //Check if the request is matched in the remote caches list and put it in the cache if it is.
   for(const remoteCacheUrl of remoteCachePathPrefixes){
     if(event.request.url.startsWith(remoteCacheUrl)){
-      return caches.open(remoteCacheName).then(cache => {
-        return cache.match(event.request).then(response => {
-          if (response) return response;
-
-          return fetch(event.request, {
-              mode: 'no-cors'
-            })
-            .then(networkResponse => {
-              cache.put(event.request, networkResponse.clone());
-              return networkResponse;
-            });
-        });
-      });
+      return event.respondWith(serveRemoteFile(event));
     }
   }
 
   if (requestUrl.origin === location.origin) {
     //If the requested page is restaurant.html, serve the empty restaurant info page
     if (requestUrl.pathname === '/restaurant.html') {
-      return event.respondWith(caches.match('restaurant.html')
-        .then(response => {
-          return response || fetch(event.request);
-      }));
+      return event.respondWith(serveRestaurantPage(event));
     }
 
     //Check if the path is for restaurant images and try to get them from cache if they are.
     if(requestUrl.pathname.startsWith('/img/')){
-      return event.respondWith(
-        caches.open(imagesCacheName).then(cache => {
-          return cache.match(event.request).then(response => {
-            if (response) return response;
-
-            return fetch(event.request)
-              .then(imageResponse => {
-                cache.put(event.request, imageResponse.clone());
-                return imageResponse;
-              });
-          });
-        }));
+      return event.respondWith(serveRestaurantImage(event));
     }
   }
 
@@ -112,3 +87,49 @@ self.addEventListener('activate', event => {
     })
   );
 });
+
+/**
+ * Serve cached remote files.
+ */
+const serveRemoteFile = (event) => {
+  return caches.open(remoteCacheName).then(cache => {
+    return cache.match(event.request).then(response => {
+      if (response) return response;
+
+      return fetch(event.request, {
+          mode: 'no-cors'
+        })
+        .then(networkResponse => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+    });
+  });
+}
+
+/**
+ * Serve cached restaurant page.
+ */
+const serveRestaurantPage = (event) => {
+  return caches.match('restaurant.html')
+        .then(response => {
+          return response || fetch(event.request);
+      })
+}
+
+/**
+ * Serve cached restaurant images.
+ */
+const serveRestaurantImage = (event) => {
+  return caches.open(imagesCacheName).then(cache => {
+      return cache.match(event.request).then(response => {
+        if (response) return response;
+
+        return fetch(event.request)
+          .then(imageResponse => {
+            cache.put(event.request, imageResponse.clone());
+            return imageResponse;
+          });
+      });
+    });
+}
